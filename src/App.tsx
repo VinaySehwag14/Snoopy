905c204417ae60b1d713732e89a1febb9953ff6c
VinaySehwag14 <vinaysehwag14@gmail.com>
2024-07-02 12:28:11 +0530

    setted up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
 import "./globals.css";
+import RootLayout from "./root/RootLayout";
+import { Home } from "./root/pages";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-white-700 underline">Hello</h1>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="sign-in" element={<SigninForm />} />
+          <Route path="sign-up" element={<SignupForm />} />
+        </Route>
+        {/* private routes */}
+        <Route element={<RootLayout />} />
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   );
 }
 
